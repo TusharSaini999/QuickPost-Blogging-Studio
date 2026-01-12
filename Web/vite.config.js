7b3618be2168d7df0a606e9f0bd1a5c078e3c3c4
Tusharsaini999 <tusharsaini.in@gmail.com>
2026-01-12 16:43:57 +0530

    Configer vite

--- Web/vite.config.js
+++ Web/vite.config.js
@@ -1,28 +1,28 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-// // https://vite.dev/config/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+// https://vite.dev/config/
 
-// export default defineConfig({
-//   plugins: [react(),tailwindcss(),],
-// })
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import svgr from "vite-plugin-svgr";
 export default defineConfig({
-  plugins: [
-    react(), 
-    tailwindcss(),
-    svgr({
-      svgrOptions: {
-        icon: true,
-      },
-    }),],
-  server: {
-    host: true,       // Listen on all addresses, including LAN
-    open: true,       // Automatically open in browser
-    https: false      // Set to true if you want HTTPS
-  }
-});
+  plugins: [react(),tailwindcss(),],
+})
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import tailwindcss from '@tailwindcss/vite';
+// import svgr from "vite-plugin-svgr";
+// export default defineConfig({
+//   plugins: [
+//     react(), 
+//     tailwindcss(),
+//     svgr({
+//       svgrOptions: {
+//         icon: true,
+//       },
+//     }),],
+//   server: {
+//     host: true,       // Listen on all addresses, including LAN
+//     open: true,       // Automatically open in browser
+//     https: true      // Set to true if you want HTTPS
+//   }
+// });
