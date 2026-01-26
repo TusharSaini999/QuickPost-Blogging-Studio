ec9dd4e172839115b36e6c7092f0579fd467d54b
Tusharsaini999 <tusharsaini.in@gmail.com>
2026-01-26 12:16:27 +0530

    Refactor store configuration to use a combined root reducer for improved maintainability and Clear the store

--- Web/src/Store/Store.js
+++ Web/src/Store/Store.js
@@ -1,17 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit"
-import TheamSlice from "../Feature/TheamMode";
-import AuthSlice from "../Feature/Auth";
-import ProfileSlice from "../Feature/Profile";
-import PostSlice from "../Feature/Post";
-import GobalSlice from "../Feature/GlobalPost";
+import RootReducer from "../Feature/rootReducer";
 const store = configureStore({
-  reducer: {
-    TheamSlice,
-    AuthSlice,
-    ProfileSlice,
-    PostSlice,
-    GobalSlice,
-  }
+  reducer: RootReducer
 });
 
 export default store
